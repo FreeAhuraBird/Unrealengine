87044189dd6e42c0bc2a24e46ed792d5955447e8
FreeAhuraBird <hakimipegah@gmail.com>
2024-11-20 00:17:07 +0100

    Adding methods to get actor and player data
    Adding GetAllActorData() and GetPlayerData()

--- Source/project/projectGameMode.cpp
+++ Source/project/projectGameMode.cpp
@@ -3,12 +3,80 @@
 #include "projectGameMode.h"
 #include "projectCharacter.h"
 #include "UObject/ConstructorHelpers.h"
+#include "UObkect/ConstructorHelpers.h"
+#include "GameFramework/Actor.h"
+#include "EngineUtils.h"
 
 AprojectGameMode::AprojectGameMode()
-	: Super()
+	: Super() // super() is part of C++ constructor initialzation, specifically for calling the constructor of the parent (or base) class.
+              // in this scenario Super() refers to AGameModeBase
 {
 	// set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
 	DefaultPawnClass = PlayerPawnClassFinder.Class;
 
+}
+
+void AprojectGameMode::BeginPlay()
+{
+    Super::BeginPlay
+     
+    // collect all actor data
+    GetAllActorData();
+    
+    // collect player-specific data
+    GetAllPlayerData();
+}
+
+void AprokectGameMode::GetAllActorData()
+{
+    //UWorld is UE's class representing the game world. A pointer is
+    // used becuase GetWorld() returns a pointer to the current world object
+    //Get World() ==> gets the current game world where all actors exist
+    UWorld* World = GetWorld();
+    
+    /*
+     if World is null or not valid, the function is stopped
+     */
+    if (!World)
+    {
+        // LogTemp is a temporary log category for general messages
+        // why warning: to make the log more clear
+        UE_LOG(LogTemp, Warning, TEXT("World context is invalid"));
+        return; // exit function to prevent crash
+    }
+    
+    /*
+     iterate thorugh all actors in the world:
+     - TActoriterator<AActor> is a UE iterator used to loop through
+     all actors of a specified type. here its AActor
+     AActor => The base class for all objects that can exist in the
+     game world.
+     
+     ActorItr(World) => Initializes the iterator to work with the
+     provided World. It will iterate through all actors in that world.
+
+     Iteration is done in the for looop below:
+     */
+    for (TActorIterator<AActor> ActorItr(World); ActorIts; ++ActorItr)
+    {
+        /* Below we use a pointer to the current actor being iterated.
+         *ActorItr - dereferences the iterator to access the actual
+         actor object
+         */
+        AActor* Actor = *ActorItr
+        
+        /*Get actor name and location:
+         
+         ActorName: Retrieves the name of the actor as a Fstring
+         ActorLocation: Returns the actors location in world space as an FVector (3d coordinate with x, y, z)
+         ActorClass: Returns the UClass of the actor, which is its type (for ex, StaticMeshActor or PointLight)
+         */
+        Fstring ActorName = Actor->GetName();
+        FVector ActorLocation = Actor->GetActorLocation();
+        FString ActorClass = Actor->GetClass()->GetName();
+        
+        // log the actor data for test?
+        UE_LOG(LogTemp, Log, Text("Actot Name: %S, CLass %S, Location %s"), *ActorName, *ActorClass, *ActorLocation.ToString());
+    }
 }
--- Source/project/projectGameMode.h
+++ Source/project/projectGameMode.h
@@ -9,11 +9,23 @@
 UCLASS(minimalapi)
 class AprojectGameMode : public AGameModeBase
 {
-	GENERATED_BODY()
+	GENERATED_BODY() // Constructor (the class function which is automatically called when the an object of the class is created.
+    // its primary function is to initialize the object - set up default values and allocate resources, and prepare it for use.
 
 public:
 	AprojectGameMode();
+    
+    // Function to gather and log all actor data
+    void GetAllActorData();
+    
+    // Function to gather and log player-specific data
+    void GetPlayerData();
+    
+protected:
+    // beginplay is overriden when the functions above are called
+    virtual void BeginPlay() override;
 };
 
 
 
+ 
